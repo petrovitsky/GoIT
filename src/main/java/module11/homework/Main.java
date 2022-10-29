b3ffc5faed94c85cd4dde8771a0ce2775d247e11
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-10-29 21:28:09 +0200

    Added implementation

--- src/main/java/module11/homework/Main.java
+++ src/main/java/module11/homework/Main.java
@@ -4,15 +4,24 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         List<String> list = Arrays.asList("eugene", "alex", "vika", "kolya",
                 "yana", "denis", "ivan", "olga", "serg", "anna", "tanya", "katya");
 
+        String[] data = new String[]{"1, 2, 0", "4, 5"};
+
         System.out.println(list);
         System.out.println(oddNames(list));
         System.out.println(upAndSort(list));
+        System.out.println(getSortedDigitsFromStringArray(data));
+
+
+
+
+
     }
 
     public static String oddNames(List<String> list) {
@@ -26,8 +35,17 @@ public class Main {
 
     public static List<String> upAndSort(List<String> list) {
         return list.stream()
-                .map(w -> w.toUpperCase())
+                .map(String::toUpperCase)
                 .sorted(Comparator.reverseOrder())
                 .collect(Collectors.toList());
+    }
+
+
+    public static String getSortedDigitsFromStringArray (String[] data){
+         return Arrays.stream(data)
+                .map((s -> s.split(", ")))
+                .flatMap(Stream::of)
+                .sorted()
+                .collect(Collectors.joining(", "));
     }
 }

e89e5cbd296336b1977c208b897c9aaa3543bbb5
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-11-13 18:54:52 +0100

    Added implementation

--- src/main/java/module12/ForkJoinPoolExample.java
+++ src/main/java/module12/ForkJoinPoolExample.java
@@ -1,4 +1,28 @@
 package module12;
 
+import java.util.concurrent.RecursiveAction;
+
 public class ForkJoinPoolExample {
+}
+
+
+class MyAction extends RecursiveAction{
+    private  int value;
+
+    public MyAction(int value) {
+        this.value = value;
+    }
+
+    @Override
+    protected void compute() {
+        if (value <= 4) {
+            System.out.println("Hello before" + Thread.currentThread().getId() + " " + value) ;
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Hello after" + Thread.currentThread().getId() + " " + value) ;
+        }
+    }
 }

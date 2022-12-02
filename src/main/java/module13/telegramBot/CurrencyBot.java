022a213e5ed1b527117f99de8c48abc98b056cb2
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-12-02 22:56:16 +0100

    Added implementation

--- src/main/java/module13/telegramBot/CurrencyBot.java
+++ src/main/java/module13/telegramBot/CurrencyBot.java
@@ -4,7 +4,12 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.ChatMemberUpdated;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CurrencyBot extends TelegramLongPollingBot {
     @Override
@@ -20,7 +25,7 @@ public class CurrencyBot extends TelegramLongPollingBot {
         // We check if the update has a message and the message has text
         if (update.hasMessage() && update.getMessage().hasText()) {
             SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
-            message.setChatId(update.getMessage().getChatId().toString() );
+            message.setChatId(update.getMessage().getChatId().toString());
             System.out.println("update.getMessage().getText() = " + update.getMessage().getText());
             message.setText(update.getMessage().getText().toUpperCase());
             System.out.println("update.getMessage().getChatId() = " + update.getMessage().getChatId());
@@ -28,10 +33,47 @@ public class CurrencyBot extends TelegramLongPollingBot {
             try {
                 Thread.sleep(1000);
                 execute(message); // Call method to send the message
+                sendCustomKeyboard(String.valueOf(update.getMessage().getChatId()));
             } catch (TelegramApiException | InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
+    public void sendCustomKeyboard(String chatId) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText("Custom message text");
 
+        // Create ReplyKeyboardMarkup object
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        // Create the keyboard (list of keyboard rows)
+        List<KeyboardRow> keyboard = new ArrayList<>();
+        // Create a keyboard row
+        KeyboardRow row = new KeyboardRow();
+        // Set each button, you can also use KeyboardButton objects if you need something else than text
+        row.add("Row 1 Button 1");
+        row.add("Row 1 Button 2");
+        row.add("Row 1 Button 3");
+        // Add the first row to the keyboard
+        keyboard.add(row);
+        // Create another keyboard row
+        row = new KeyboardRow();
+        // Set each button for the second line
+        row.add("Row 2 Button 1");
+        row.add("Row 2 Button 2");
+        row.add("Row 2 Button 3");
+        // Add the second row to the keyboard
+        keyboard.add(row);
+        // Set the keyboard to the markup
+        keyboardMarkup.setKeyboard(keyboard);
+        // Add it to the message
+        message.setReplyMarkup(keyboardMarkup);
+
+        try {
+            // Send the message
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
 }
